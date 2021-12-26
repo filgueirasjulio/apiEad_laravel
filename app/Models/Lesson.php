36f89e072d2a927baecad03a39dbf9d49241aaf3
filgueirasjulio <julio_filgueiras@live.com>
2021-12-25 22:16:42 -0300

    feat(aulas): model e migration

---
+++ app/Models/Lesson.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Lesson extends Model
+{
+    use HasFactory, UuidTrait;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+
+    protected $fillable = ['name', 'description', 'video'];
+}
