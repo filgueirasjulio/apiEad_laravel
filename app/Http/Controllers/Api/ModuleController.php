d46b6b2b4272f88e41b640225bf16bab85439da8
filgueirasjulio <julio_filgueiras@live.com>
2021-12-26 00:25:33 -0300

    feat(módulos): api de listagem

--- app/Http/Controllers/Api/ModuleController.php
+++ app/Http/Controllers/Api/ModuleController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ModuleResource;
+use App\Repositories\ModuleRepository;
 
 class ModuleController extends Controller
 {
-    //
+    protected $repository;
+
+    public function __construct(ModuleRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+      /**
+     * Lista módulos a partir de um curso
+     *
+     * @param string $courseId
+     * 
+     * @return collection
+     */
+    public function index($courseId)
+    {
+        return ModuleResource::collection($this->repository->getModulesByCourseId($courseId));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Api as Controller;
 Route::get('/courses', [Controller\CourseController::class, 'index']);
 Route::get('/courses/{id}', [Controller\CourseController::class, 'show']);
 
+Route::get('/courses/{id}/modules', [Controller\ModuleController::class, 'index']);
+
 Route::get('/', function() {
     return response()->json([
         'success' => true,
