99704706717e2bd86c3df9cde3abd643746d792f
filgueirasjulio <julio_filgueiras@live.com>
2022-02-16 01:28:02 -0300

    test: autenticação

---
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use Tests\TestCase;
+use App\Models\User;
+
+class AuthTest extends TestCase
+{
+    public $user;
+    public $token;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->token =  $this->user->createToken('teste')->plainTextToken;
+    }
+
+    public function test_fail_auth()
+    {
+        $response = $this->postJson('/auth', []);
+
+        $response->assertStatus(422);
+    }
+
+    public function test__auth()
+    {
+        $response = $this->postJson('/auth', []);
+
+        $response = $this->postJson('/auth', [
+            'email' => $this->user->email,
+            'password' => 'password',
+            'device_name' => 'test'
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_logout()
+    {
+        $response = $this->postJson('/logout');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_logout()
+    {
+        $response = $this->postJson('/logout', [], [
+            'Authorization' => "Bearer {$this->token}",
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_error_get_me()
+    {
+        $response = $this->getJson('/me');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_me()
+    {
+        $response = $this->getJson('/me', [
+            'Authorization' => "Bearer {$this->token}",
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
